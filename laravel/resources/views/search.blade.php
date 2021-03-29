09c4ba62324f7a53381cc683d445a501af7097cb
Wulung Wahyu Witono <wulungqwerty@gmail.com>
2021-03-29 16:21:29 +0700

    search php dan controller ref #3

--- laravel/resources/views/search.blade.php
+++ laravel/resources/views/search.blade.php
@@ -12,7 +12,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">-History-</h3>
+              <h3 class="box-title">Search</h3>
             </div>
             <div class="box-body">
               <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"></div><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
